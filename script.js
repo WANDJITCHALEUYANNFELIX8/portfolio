333da57fefa19b847876c9688f95db749065cff0
WANDJITCHALEUYANNFELIX8 <wandjiyann8@gmail.com>
2026-03-28 16:09:59 +0100

    correction progress-bar

--- script.js
+++ script.js
@@ -209,6 +209,16 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 });
 
+/*Barre de progression*/
+
+const progressBar=document.getElementById('progress-bar');
+
+window.addEventListener('scroll', () => {
+	const scrollTop=window.scrollY;
+	const docHeight=document.documentElement.scrollHeight-window.innerHeight;
+	const progress =(scrollTop / docHeight)*100;
+	progressBar.style.width=progress+'%';
+});	
 		
 		
 		
